0391a140f9fbb0f91cfacdd7636d25c4312d3aee
ahmidev <ahmid.aitouali@gmail.com>
2024-07-30 22:47:18 +0200

    Add unit tests for Account and Logout functionality: - Test user information display - Test user logout

--- front/src/app/components/me/me.component.spec.ts
+++ front/src/app/components/me/me.component.spec.ts
@@ -8,10 +8,16 @@ import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { SessionService } from 'src/app/services/session.service';
 
 import { MeComponent } from './me.component';
+import { User } from 'src/app/interfaces/user.interface';
+import { of } from 'rxjs';
+import { BrowserAnimationsModule, NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterTestingModule } from '@angular/router/testing';
+import { UserService } from 'src/app/services/user.service';
 
 describe('MeComponent', () => {
   let component: MeComponent;
   let fixture: ComponentFixture<MeComponent>;
+  let mockUserService: any;
 
   const mockSessionService = {
     sessionInformation: {
@@ -19,7 +25,24 @@ describe('MeComponent', () => {
       id: 1
     }
   }
+
+  const mockUser: User = {
+    id: 1,
+    firstName: 'John',
+    lastName: 'Doe',
+    email: 'john.doe@example.com',
+    admin: true,
+    password:'null',
+    createdAt: new Date('2024-01-01T00:00:00.000Z'),
+    updatedAt: new Date('2024-01-02T00:00:00.000Z')
+  };
+  
   beforeEach(async () => {
+    mockUserService = {
+      getById: jest.fn().mockReturnValue(of(mockUser)),
+      delete: jest.fn().mockReturnValue(of({}))
+    };
+
     await TestBed.configureTestingModule({
       declarations: [MeComponent],
       imports: [
@@ -28,9 +51,15 @@ describe('MeComponent', () => {
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
-        MatInputModule
+        MatInputModule,
+        BrowserAnimationsModule,
+        NoopAnimationsModule,
+        RouterTestingModule
       ],
-      providers: [{ provide: SessionService, useValue: mockSessionService }],
+      providers: [
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: UserService, useValue: mockUserService }
+      ],
     })
       .compileComponents();
 
@@ -41,5 +70,26 @@ describe('MeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display user information', () => {
+    fixture.detectChanges();
+
+    const compiled = fixture.nativeElement;
+
+    const nameElement = compiled.querySelector('mat-card-content p:first-child');
+    expect(nameElement.textContent).toContain('Name: John DOE');
+
+    const emailElement = compiled.querySelector('mat-card-content p:nth-child(2)');
+    expect(emailElement.textContent).toContain('Email: john.doe@example.com');
+
+    const adminElement = compiled.querySelector('mat-card-content p.my2');
+    expect(adminElement.textContent).toContain('You are admin');
+
+    const createdAtElement = compiled.querySelector('mat-card-content .p2.w100 p:first-child');
+    expect(createdAtElement.textContent).toContain('Create at:  January 1, 2024');
+
+    const updatedAtElement = compiled.querySelector('mat-card-content .p2.w100 p:last-child');
+    expect(updatedAtElement.textContent).toContain('Last update:  January 2, 2024');
   });
 });
